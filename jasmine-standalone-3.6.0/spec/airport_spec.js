b80eccfd10ca25720b207b7ecdc7ff1924f0b751
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-23 11:01:03 +0000

    RED - wrote airport test

--- jasmine-standalone-3.6.0/spec/airport_spec.js
+++ jasmine-standalone-3.6.0/spec/airport_spec.js
@@ -1,13 +1,22 @@
 describe('Airport', function() {
   var airport;
+  var plane;
 
   beforeEach(function() {
     airport = new Airport();
+    plane = new Plane();
+  });
+
+  describe('Check instance of class', function() {
+    it('exists', function() {
+      var gatwick = new Airport();
+      expect(gatwick).toBe(true);
+    });
   });
 
   describe('#land', function() {
     it('informs air traffic controller when plane has landed', function() {
-      
+      expect(airport.land(plane)).toEqual("");
     });
   });
 
